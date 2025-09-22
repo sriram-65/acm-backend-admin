58aed12383f6f33516ff7cc1af13e7b3701176a1
sriram <159551306+sriram-65@users.noreply.github.com>
2025-09-22 21:48:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,41 +13,31 @@ import os
 
 app = Flask(__name__)
 
-# ----------------------------
-# Cookie-based session config
-# ----------------------------
+
 app.secret_key = os.getenv("SECERT_KEY")
 app.permanent_session_lifetime = timedelta(days=50)
 
 app.config.update(
-    SESSION_COOKIE_SAMESITE="None", # Needed for cross-origin
-    SESSION_COOKIE_SECURE=True,     # HTTPS required
+    SESSION_COOKIE_SAMESITE="None",
+    SESSION_COOKIE_SECURE=True,    
     SESSION_PERMANENT=True
 )
 
-# ----------------------------
-# CORS setup
-# ----------------------------
+
 CORS(app,
-     resources={r"/*": {"origins": "https://acm-admin-frontend.vercel.app"}},
+     
      supports_credentials=True)
 
-# ----------------------------
-# Load environment variables
-# ----------------------------
+
 load_dotenv()
 
-# ----------------------------
-# Register Blueprints
-# ----------------------------
+
 app.register_blueprint(ERoutes , url_prefix="/api/events")
 app.register_blueprint(AuthRoutes , url_prefix="/api/auth")
 app.register_blueprint(GalleryRoutes , url_prefix='/api/gallery')
 app.register_blueprint(OutReach , url_prefix='/api/outreach')
 app.register_blueprint(RecentEvents , url_prefix='/api/recent')
 
-# ----------------------------
-# Run the app
-# ----------------------------
+
 if __name__ == "__main__":
     app.run(debug=True)
